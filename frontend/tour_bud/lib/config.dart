a342a5ce4cbff32e7f5c4fec2cd34cb982a11a86
M H K Viduranga <kavinduv69@gmail.com>
2026-04-23 12:29:10 +0530

    Developed the My Trips page and more enhancements to dashboard

--- frontend/tour_bud/lib/config.dart
+++ frontend/tour_bud/lib/config.dart
@@ -1,3 +1,13 @@
 class AppConfig {
   static const String baseUrl = 'http://66.154.127.192:5000';
+  static String? authToken;
+  static String? userFullName;
+
+  static Map<String, String> get authHeaders {
+    if (authToken == null) return {};
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $authToken',
+    };
+  }
 }
